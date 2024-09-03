afa672c2e299dec6a91c914ecc0f1f07dca96c6e
Adam Lynch <adamclynch03@gmail.com>
2024-09-03 19:46:39 -0400

    fix: driver

--- cboescraper/scraper.py
+++ cboescraper/scraper.py
@@ -2,100 +2,22 @@ from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
-import elements
+from cboescraper import elements
 import os
-import datetime
-import pandas
-
-# DB_DIR = "cboe_options"
-
-CBOE_FILE_ENDING = "_quotedata.csv"
-
-CBOE_START_DATE = datetime.datetime(1970, 1, 1)
-
-DEFAULT_DF = pandas.DataFrame(
-    {
-        "Expiration": [],
-        "CBid": [],
-        "CAsk": [],
-        "CVolumne": [],
-        "COI": [],
-        "Strike": [],
-        "PBid": [],
-        "PAsk": [],
-        "PVolume": [],
-        "POI": [],
-    }
-)
 
 
-def ticker_format(ticker):
+CBOE_FILE_ENDING = "_quotedata"
+
+
+def _ticker_format(ticker):
     return ticker.lower().replace("-", ".")
 
 
-# def get_options(ticker, date, db_name):
-#     ticker = ticker_format(ticker)
-#     date_str = str(date.year) + str(date.month).zfill(2) + str(date.day).zfill(2)
-#     path = db_name + "/" + DB_DIR + "/" + ticker + date_str + ".csv"
-#     if os.path.isfile(path):
-#         df = pandas.read_csv(path)
-#         df["Expiration"] = CBOE_START_DATE + pandas.to_timedelta(
-#             df["Expiration"], unit="d"
-#         )
-#         return df
-#     else:
-#         return DEFAULT_DF
-
-
-# def download_tickers_to_db(tickers, db_name, downloads_dir, wait_time=1):
-#     driver = Driver()
-#     return driver.download_tickers_to_db(tickers, db_name, downloads_dir, wait_time)
-
-
-# def parse_df(df):
-#     df = df.drop(
-#         [
-#             "Calls",
-#             "Last Sale",
-#             "Net",
-#             "IV",
-#             "Delta",
-#             "Gamma",
-#             "Puts",
-#             "Last Sale.1",
-#             "Net.1",
-#             "IV.1",
-#             "Delta.1",
-#             "Gamma.1",
-#         ],
-#         axis=1,
-#     )
-#     df = df.rename(
-#         columns={
-#             "Expiration Date": "Expiration",
-#             "Bid": "CBid",
-#             "Ask": "CAsk",
-#             "Volume": "CVolume",
-#             "Open Interest": "COI",
-#             "Bid.1": "PBid",
-#             "Ask.1": "PAsk",
-#             "Volume.1": "PVolume",
-#             "Open Interest.1": "POI",
-#         }
-#     )
-#     df["Expiration"] = pandas.to_datetime(df["Expiration"], format="%a %b %d %Y")
-#     df["Expiration"] = (df["Expiration"] - CBOE_START_DATE).dt.days
-#     return df
-
-
-# def update_db_from_downloads(ticker, downloads_dir, db_name, date):
-#     downloads_path = downloads_dir + ticker + CBOE_FILE_ENDING
-#     df = pandas.read_csv(downloads_path, header=2)
-#     df = parse_df(df)
-#     date_str = str(date.year) + str(date.month).zfill(2) + str(date.day).zfill(2)
-#     db_path = db_name + "/" + DB_DIR + "/" + ticker + date_str + ".csv"
-#     df.to_csv(db_path, index=False)
-#     os.remove(downloads_path)
+def _tickers_format(tickers):
+    tickers_new = []
+    for i in range(0, len(tickers)):
+        tickers_new.append(_ticker_format(tickers[i]))
+    return tickers_new
 
 
 class Driver:
@@ -106,75 +28,99 @@ class Driver:
     def __del__(self):
         self.driver.quit()
 
-    def get_ticker(self, ticker):
+    def _get_ticker(self, ticker):
         LINK1 = "https://www.cboe.com/delayed_quotes/"
         LINK2 = "/quote_table"
         url = LINK1 + ticker + LINK2
         self.driver.get(url)
 
-    def find_selectors(self):
+    def _find_selectors(self):
         self.selectors = self.driver.find_elements(
             By.CLASS_NAME, elements.SELECTOR_CLASS
         )
 
-    def scroll_element_into_view(self, element):
+    def _scroll_element_into_view(self, element):
         self.driver.execute_script("arguments[0].scrollIntoView();", element)
         self.driver.execute_script("window.scrollBy(0,-100)")
 
-    def set_selectors(self):
+    def _set_selectors(self):
         for selector in self.selectors:
             input = selector.find_element(By.CLASS_NAME, elements.INPUT_CONTAINER)
-            self.scroll_element_into_view(input)
+            self._scroll_element_into_view(input)
             input.send_keys("All")
             input.send_keys(Keys.ENTER)
 
-    def view_chain(self, wait_time=1):
+    def _view_chain(self, wait_time=0.3):
         button = self.driver.find_element(By.CLASS_NAME, elements.VIEW_CHAIN_BUTTON)
-        self.scroll_element_into_view(button)
+        self._scroll_element_into_view(button)
         button.click()
         time.sleep(wait_time)
 
-    def download(self, wait_time=1):
+    def _download(self):
         link = self.driver.find_element(By.CLASS_NAME, elements.DOWNLOAD_LINK)
-        self.scroll_element_into_view(link)
+        self._scroll_element_into_view(link)
         link.click()
-        time.sleep(wait_time)
 
-    def verify_download(sefl, ticker, downloads_dir):
-        path = downloads_dir + ticker + CBOE_FILE_ENDING
-        return os.path.exists(path)
-
-    def download_ticker(self, ticker, downloads_dir, wait_time=1):
-        self.get_ticker(ticker)
-        self.find_selectors()
-        self.set_selectors()
-        self.view_chain(wait_time)
-        self.download(wait_time)
-        # if not self.verify_download(ticker, downloads_dir):
-        #     raise Exception()
+    def download_ticker(self, ticker, load_time):
+        self._get_ticker(ticker)
+        self._find_selectors()
+        self._set_selectors()
+        self._view_chain(load_time)
+        self._download()
 
 
-driver = Driver()
-driver.download_ticker("aapl", None)
+def _exists(ticker, downloads):
+    path = downloads + ticker + CBOE_FILE_ENDING + ".csv"
+    return os.path.exists(path)
 
-# def download_tickers_to_db(self, tickers, db_name, downloads_dir, wait_time=1):
-#     failed = []
-#     #data = data_logs.read_log(db_name, data_logs.CBOE_OPTION_LOG)
-#     today = datetime.datetime.strftime(datetime.datetime.today(), "%Y%m%d")
-#     for ticker in tickers:
-#         ticker = ticker_format(ticker)
-#         if ticker in data:
-#             if today in data[ticker]:
-#                 continue
-#         try:
-#             self.download_ticker(ticker, downloads_dir, wait_time)
-#             update_db_from_downloads(
-#                 ticker, downloads_dir, db_name, datetime.datetime.today()
-#             )
-#             if ticker not in data.keys():
-#                 data[ticker] = []
-#             data[ticker].append(today)
-#         except:
-#             failed.append(ticker)
-#     #data_logs.write_log(db_name, data, data_logs.CBOE_OPTION_LOG)
-#     return failed
+
+def _apt_size(ticker, downloads, size):
+    path = downloads + ticker + CBOE_FILE_ENDING + ".csv"
+    return os.path.getsize(path) > size
+
+
+def _rm_file(ticker, downloads):
+    path = downloads + ticker + CBOE_FILE_ENDING + ".csv"
+    os.remove(path)
+
+
+def _download_tickers(driver, tickers, load_time):
+    for ticker in tickers:
+        try:
+            driver.download_ticker(ticker, load_time)
+        except:
+            pass
+
+
+KB = 1024
+MB = 1024 * KB
+APT_SIZE = 10 * KB
+
+
+def _failed(tickers, downloads):
+    failed = []
+    for ticker in tickers:
+        if not _exists(ticker, downloads):
+            failed.append(ticker)
+        elif not _apt_size(ticker, downloads, APT_SIZE):
+            _rm_file(ticker, downloads)
+            failed.append(ticker)
+    return failed
+
+
+LOAD_FAST = 0.5
+LOAD_SLOW = 3
+
+DOWNLOAD_WAIT_TIME = 5
+
+
+def download_tickers(tickers, downloads):
+    tickers = _tickers_format(tickers.copy())
+    driver = Driver()
+    _download_tickers(driver, tickers, LOAD_FAST)
+    time.sleep(DOWNLOAD_WAIT_TIME)
+    failed = _failed(tickers, downloads)
+    _download_tickers(driver, failed, LOAD_SLOW)
+    time.sleep(DOWNLOAD_WAIT_TIME)
+    failed = _failed(failed, downloads)
+    return failed
